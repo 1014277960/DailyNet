728c9b3fe26b5b54272203886366a0f962ce7952
wulingpeng <1014277960@qq.com>
2017-04-27 19:00:07 +0800

    完善代码 增加代码复用

--- app/src/main/java/com/wiipu/dailynet/core/DailyNet.java
+++ app/src/main/java/com/wiipu/dailynet/core/DailyNet.java
@@ -29,6 +29,13 @@ public class DailyNet {
         return Singleton.INSTANCE.getInstance();
     }
 
+    /**
+     * 返回动态代理对象，调用方法时解析注解，生成Request，然后调用createCall生成call返回
+     * @param service
+     * @param context
+     * @param <T>
+     * @return
+     */
     public <T> T create(Class<T> service, final Context context) {
         return (T) Proxy.newProxyInstance(service.getClassLoader(), new Class<?>[]{service},
                 new InvocationHandler() {
@@ -44,65 +51,39 @@ public class DailyNet {
                 });
     }
 
-    /**
-     * 返回动态代理对象，调用方法时解析注解，生成Request，然后调用createCall生成call返回
-     * @param method
-     * @param args
-     * @return
-     */
     private Request parseMethod(Method method, Object[] args) {
         Request request = null;
         Annotation[] annotations = method.getAnnotations();
         for (Annotation annotation : annotations) {
-            // 解析GET请求
-            if (annotation instanceof GET) {
-                request = getRequest(method, args, annotation);
+            // 解析GET或POST请求
+            if (annotation instanceof GET || annotation instanceof POST) {
+                request = buildRequest(method, args, annotation);
                 break;
-            } else if (annotation instanceof POST) {
-                request = postRequest(method, args, annotation);
             }
+        }
+        if (request == null) {
+            throw new RuntimeException("Interface annotation error!");
         }
         return request;
     }
 
-    private Request getRequest(Method method, Object[] args, Annotation methodAnnotation) {
+    /**
+     * 根据注解与参数构造Request
+     * @param method
+     * @param args
+     * @param methodAnnotation
+     * @return
+     */
+    private Request buildRequest(Method method, Object[] args, Annotation methodAnnotation) {
         Request.Builder builder = new Request.Builder();
-        builder.method(Request.Method.GET);
-        String url = ((GET) methodAnnotation).value();
-        Type[] types = method.getGenericParameterTypes();
-        Annotation[][] paramAnnotations = method.getParameterAnnotations();
-        RequestParam param = new RequestParam();
-
-        // 解析参数
-        for (int i = 0; i != paramAnnotations.length; ++i) {
-            Annotation[] paramAnnotation = paramAnnotations[i];
-            if (paramAnnotation.length == 0) {
-                continue;
-            }
-            Annotation a = paramAnnotation[0];
-            // 替换url中的{path}
-            if (a instanceof Path) {
-                String key = ((Path) a).value();
-                url = url.replaceFirst("\\{" + key + "\\}", String.valueOf(args[i]));
-            } else if (a instanceof Query) {
-                // 添加参数，不直接添加到url后面，作为成员变量传入，转换url交给GetStrategy来做
-                String key = ((Query) a).value();
-                Object value = args[i];
-                if (value instanceof Integer || value instanceof String) {
-                    param.addParam(key, String.valueOf(value));
-                } else {
-                    throw new RuntimeException("HTTP param type error!");
-                }
-            }
+        String url = "";
+        if (methodAnnotation instanceof GET) {
+            builder.method(Request.Method.GET);
+            url = ((GET) methodAnnotation).value();
+        } else if (methodAnnotation instanceof POST) {
+            builder.method(Request.Method.POST);
+            url = ((POST) methodAnnotation).value();
         }
-        builder.url(url).param(param);
-        return builder.build();
-    }
-
-    private Request postRequest(Method method, Object[] args, Annotation methodAnnotation) {
-        Request.Builder builder = new Request.Builder();
-        builder.method(Request.Method.POST);
-        String url = ((POST) methodAnnotation).value();
         Type[] types = method.getGenericParameterTypes();
         Annotation[][] paramAnnotations = method.getParameterAnnotations();
         RequestParam param = new RequestParam();
@@ -128,6 +109,7 @@ public class DailyNet {
                     throw new RuntimeException("HTTP param type error!");
                 }
             }
+
         }
         builder.url(url).param(param);
         return builder.build();
