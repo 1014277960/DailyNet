fdcd96d124c62df61ea285a7ab94e80096d85790
wulingpeng <1014277960@qq.com>
2018-03-01 22:47:12 +0800

    todo

--- app/src/main/java/com/wiipu/dailynet/converter/ConverterFactory.java
+++ app/src/main/java/com/wiipu/dailynet/converter/ConverterFactory.java
@@ -4,6 +4,7 @@ package com.wiipu.dailynet.converter;
  * @author wulinpeng
  * @datetime: 18/3/1 下午10:20
  * @description: 由于Converter有范型，需要转换为具体的类型，所以创建Converter的时候需要传入真正的类型，由factory来做
+ * todo 参考retrofit重新架构，了解type相关内容
  */
 public interface ConverterFactory<F> {
 
